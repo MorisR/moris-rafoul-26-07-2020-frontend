fb86bd46e03d960d3c1d58082389ab4b0b6396ac
moris <moris.rafol@gmail.com>
2020-07-30 01:36:10 +0300

    feat: refine classes

--- src/classes/MessageData.js
+++ src/classes/MessageData.js
@@ -1,12 +1,14 @@
-import {UserData} from "./UserData";
+import UserData from "./UserData";
+import moment from "moment";
 
 export default class MessageData {
     #title
     #content
     #sender
     #receiver
+    #creationDate
 
-    constructor({title, content = "", sender, receiver}) {
+    constructor({title, content = "", sender, receiver,creationDate}) {
 
         if (!sender instanceof UserData)
             throw new Error("sender must be of type UserData")
@@ -19,31 +21,38 @@ export default class MessageData {
 
         if (typeof title !== "string")
             throw new Error("title must be of type string")
+        if (!creationDate instanceof Date)
+            throw new Error("creationDate must be of type Date")
 
-        this.title = title;
-        this.content = content;
-        this.sender = sender;
-        this.receiver = receiver;
-
+        this.#title = title;
+        this.#content = content;
+        this.#sender = sender;
+        this.#receiver = receiver;
+        this.#creationDate = creationDate
     }
 
     get title() {
-        return this.title
+        return this.#title
     }
 
     get content() {
-        return this.content
+        return this.#content
     }
 
     get sender() {
-        return this.sender
+        return this.#sender
     }
 
     get receiver() {
-        return this.receiver
+        return this.#receiver
+    }
+    get creationDate() {
+        return this.#creationDate
     }
 
-
+    get creationDateFromNow() {
+        return moment(this.#creationDate).toNow()
+    }
 }
 
 
--- src/classes/UserData.js
+++ src/classes/UserData.js
@@ -14,35 +14,37 @@ export default class UserData {
         if (typeof lastName !== "string")
             throw new Error("content must be of type string")
 
-        if (typeof id !== "string" || typeof id !== "number")
+        if (typeof id !== "string" && typeof id !== "number")
             throw new Error("content must be of type string or number")
 
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.id = id;
+        this.#email = email;
+        this.#firstName = firstName?.[0].toUpperCase() + firstName.slice(1);
+        this.#lastName = lastName?.[0].toUpperCase() + lastName.slice(1);
+        this.#id = id;
     }
 
     get email() {
-        return this.email
+        return this.#email
     }
 
     get firstName() {
-        return this.firstName
+        return this.#firstName
     }
 
     get lastName() {
-        return this.lastName
+        return this.#lastName
     }
 
     get id() {
-        return this.id
+        return this.#id
     }
 
     get fullName() {
-        return `${this.firstName} ${this.lastName}`
+        return `${this.#firstName} ${this.#lastName}`
     }
-
+    get nameInitials() {
+        return `${this.#firstName?.[0]}${this.#lastName?.[0]}`
+    }
     isCurrentUser(loggedInUserId) {
         return this.id === loggedInUserId;
     }
