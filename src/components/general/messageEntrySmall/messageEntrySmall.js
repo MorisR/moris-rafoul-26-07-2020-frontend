918d00b56a33259a35672d4b7893509e4de11350
moris <moris.rafol@gmail.com>
2020-07-30 01:42:58 +0300

    fix: remove commented out and unused code

--- src/components/general/messageEntrySmall/messageEntrySmall.js
+++ src/components/general/messageEntrySmall/messageEntrySmall.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Card, Typography, Avatar, Grid, Box} from '@material-ui/core';
 import useStyle from "./messagesEntrySmall.style"
-import UserData from "../../../classes/UserData";
-import MessageData from "../../../classes/MessageData";
 
 
 
@@ -24,47 +22,5 @@ function MessageEntrySmall({messageData}) {
     );
 }
 
-
-//
-// function MessageEntrySmall({messageData = defaultMessageData}) {
-//
-//     const cssClasses = useStyle()
-//
-//     return (<Card className={cssClasses.root} variant={"elevation"} raised={1}>
-//
-//             <Grid container  alignItems={"center"} justify={"space-evenly"}>
-//                 <Grid  item xs={1}  >
-//                     <Box className={cssClasses.centerContent}>
-//                         <Avatar className={cssClasses.icon}>{messageData?.sender?.nameInitials}</Avatar>
-//                     </Box >
-//                 </Grid>
-//                 <Grid xs={11} >
-//                     <Box >
-//
-//                         <Typography variant={"h6"}> {messageData?.sender?.fullName}</Typography>
-//                     </Box >
-//                 </Grid>
-//                 <Grid  item xs={1}/>
-//                 <Grid xs={11} >
-//                     <Box >
-//                         <Typography variant={"subtitle1"}> {messageData?.title}</Typography>
-//                     </Box >
-//                 </Grid>
-//                 <Grid  item xs={1}/>
-//                 <Grid xs={11} >
-//                     <Box >
-//                         <Typography variant={"caption"}> {messageData?.content}</Typography>
-//                     </Box >
-//                 </Grid>
-//
-//
-//
-//
-//
-//             </Grid>
-//         </Card>
-//
-//     );
-// }
 
 export default MessageEntrySmall;
