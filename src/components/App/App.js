214d1ce4584ec3f3f85812cd0c842338b64160c7
moris <moris.rafol@gmail.com>
2020-07-30 22:30:26 +0300

    feat: add dashboard screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,8 @@ import RouteData from "../../modules/classes/RouteData";
 //components-----------------------------------------------------------------------
 import RegisterScreen from "../screens/registerScreen/registerScreen";
 import LoginScreen from "../screens/loginScreen/loginScreen";
+import DashboardScreen from "../screens/dashboardScreen/dashboardScreen";
+
 import PopupMessage from "../general/popupMessage/popupMessage";
 import {routes} from "../../modules/constants";
 import {authApi} from "../../modules/api";
@@ -34,13 +36,13 @@ function App() {
     const routesDataArr = [
         new RouteData(routes.REGISTER, <RegisterScreen onRegister={LoginAfterRegister}/>),
         new RouteData(routes.LOGIN, <LoginScreen onLogin={()=>history.push(routes.DASHBOARD)}/>),
-        // new RouteData(routes.DASHBOARD,<RegisterScreen/>),
+        new RouteData(routes.DASHBOARD,<DashboardScreen/>),
         // new RouteData(routes.REGISTER,<RegisterScreen/>),
     ]
 
     function addRoute({path, component}) {
-        return <Route exact path={path} render={()=>
-                {component}
+        return <Route key={path} exact path={path} render={()=>
+                <>{component}</>
         }/>
 
 
---
+++ src/components/screens/dashboardScreen/dashboardScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import NavBar from "../../general/navBar/navBar";
+import MessagesList from "../../general/messagesEntryList/messagesList";
+import UserData from "../../../modules/classes/UserData";
+import MessageData from "../../../modules/classes/MessageData";
+import useStyle from "../../general/messagesEntryList/messagesList.style";
+
+const user = new UserData({email:"test@gmail.com", firstName:"moris", lastName:"rafoul",id:1})
+const message = new MessageData({title:"this is a test", sender:user, receiver:user,id:1})
+const messagesArr = [
+    message,
+    message,
+    message,
+    message,
+    message,
+    message,
+    message,
+    message,
+    message,
+]
+function DashboardScreen(props) {
+    const cssClasses = useStyle()
+
+    return (
+
+            <Grid container className={cssClasses.root}>
+                    <Grid item md={2}>
+                    <NavBar/>
+                </Grid>
+                <Grid item xs={3} ms={4}>
+                    <MessagesList messages={messagesArr}/>
+                </Grid>
+                <Grid item xs={0} ms={5} >
+
+                </Grid>
+
+            </Grid>
+
+
+
+    );
+}
+
+export default DashboardScreen;
