3832d355a968f349209d97cb84688356c3f154d9
moris <moris.rafol@gmail.com>
2020-07-30 20:19:04 +0300

    feat: add routes to constants

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -2,5 +2,13 @@
 export const recoilKeys = {
     SELECTED_MESSAGE_STATE : "selectedMessage",
     SELECTED_NAV_BAR_ITEM: "selectedNavBarItem",
-    LOGGED_IN_USER_STATE: "LOGGED_IN_USER_STATE"
+    LOGGED_IN_USER_STATE: "LOGGED_IN_USER_STATE",
+    POPUP_MESSAGE_STATE: "popupMessageState"
+}
+
+export const routes = {
+    REGISTER: "/register",
+    LOGIN: "/login",
+    HOME: "/",
+    DASHBOARD: "/dashboard"
 }
